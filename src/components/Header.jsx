ce8017d68b2666e1fad9d87367b7d193fdd5381f
Your Name <fantastizeey@gmail.com>
2024-07-10 13:20:39 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="/log.png" alt="logo" />
+      </div>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Shop</a>
+        <a href="/">Staff</a>
+        <a href="/">Blog</a>
+        <a href="/">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
